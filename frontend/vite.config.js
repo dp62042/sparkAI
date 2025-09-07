73382191c68069c25a61b6721cb6d5e3e9c5921a
dp62042 <dharmendra193728@gmail.com>
2025-09-07 23:20:06 +0530

    update proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_KEY,
+        target: process.env.VITE_API_KEY,
         secure: false,
       },
     },
